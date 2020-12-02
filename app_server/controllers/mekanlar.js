4220dc06eaaff304e7d109309b289a824fc02569
Furkantarhan <furkantarhan11@hotmail.com>
2020-12-02 16:30:16 +0300

    2.Odev

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,9 +1,106 @@
+var express = require('express');
+var router = express.Router();
 const anaSayfa = function (req, res, next) {
-  res.render('mekanlar-liste', { title: 'Ana Sayfa' });
+  res.render('mekanlar-liste',
+    {
+      'baslik': 'Anasayfa',
+      'sayfaBaslik': {
+        'siteAd': 'Mekan32',
+        'aciklama': '  Isparta Civarında Mekanları Keşfedin :)'
+      },
+      'mekanlar': [
+        {
+          'ad': 'Starbucks',
+          'adres': 'Centrum Garden',
+          'puan': '3',
+          'imkanlar': ['Kahve', 'Çay', 'Pasta'],
+          'mesafe': '10km'
+        },
+        {
+          'ad': 'Coffê Nero',
+          'adres': 'Iyaş AVM',
+          'puan': '4',
+          'imkanlar': ['Internet', 'WC', 'Kahve'],
+          'mesafe': '11km'
+        },
+        {
+          'ad': 'Burç Fırın',
+          'adres': 'Süleyman Demirel Caddesi',
+          'puan': '5',
+          'imkanlar': ['Kahvaltı', 'Çay', 'Pasta'],
+          'mesafe': '10km'
+        },
+        {
+          'ad': 'Uğur Pide',
+          'adres': 'Sanayi Mahallesi',
+          'puan': '5',
+          'imkanlar': ['Pide', 'Kebap', 'Sucuk'],
+          'mesafe': '22km'
+        },
+        {
+          'ad': 'Edwards Coffee',
+          'adres': 'Kafeler Sokağı',
+          'puan': '4',
+          'imkanlar': ['Kahve', 'Çay', 'Pasta'],
+          'mesafe': '14km'
+        },
+        {
+          'ad': 'Mackbear Coffee',
+          'adres': '4504 Sokak',
+          'puan': '2',
+          'imkanlar': ['kahve', 'Wi-Fi', 'Pasta'],
+          'mesafe': '8km'
+        }     
+      ]
+    }
+  );
 }
 const mekanBilgisi = function (req, res, next) {
-  res.render('mekan-detay', { title: 'Mekan Bilgisi' });
+  res.render('mekan-detay', { 
+      'baslik':'Mekan Bilgisi',
+      'sayfaBaslik':'Starbucks',
+      'mekanBilgisi':{
+        'ad':'Starbucks',
+        'adres':'Centrum Garden',
+        'puan':3,
+        'imkanlar':['Kahve','Paste','Kek'],
+        'koordinatlar':{
+          'enlem':37.781885,
+          'boylam':30.566034
+        },
+        'saatler':[
+          {
+             'gunler':'Pazartesi-Cuma',
+             'acilis':'7.00',
+             'kapanis':'23.00',
+             'kapali':false
+          },
+          {
+            'gunler':'Cumartesi',
+            'acilis':'9.00',
+            'kapanis':'22.30',
+            'kapali':false
+          },
+          {
+            'gunler':'Pazar',         
+            'kapali':true
+          }
+      ],
+      'yorumlar':[
+          {
+            'yorumYapan':'Furkan Tarhan',
+            'puan':3,
+            'tarih':'11 Mayıs 2020',
+            'yorumMetni':'Kahveler Enfesss'
+          }
+       ]
+    }
+    
+    
+  });
 }
+
+
 const yorumEkle = function (req, res, next) {
   res.render('yorum-ekle', { title: 'Yorum Ekle' });
 }
