211cd1a7bbdb3c71abc13520cfe1bc48d309b766
Furkantarhan <furkantarhan11@hotmail.com>
2020-12-04 00:09:47 +0300

    2.Ödev

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -4,6 +4,7 @@ const anaSayfa = function (req, res, next) {
   res.render('mekanlar-liste',
     {
       'baslik': 'Anasayfa',
+      'footer': 'Furkan Tarhan 2020',
       'sayfaBaslik': {
         'siteAd': 'Mekan32',
         'aciklama': '  Isparta Civarında Mekanları Keşfedin :)'
@@ -50,65 +51,69 @@ const anaSayfa = function (req, res, next) {
           'puan': '2',
           'imkanlar': ['kahve', 'Wi-Fi', 'Pasta'],
           'mesafe': '8km'
-        }     
+        }
       ]
     }
   );
 }
 const mekanBilgisi = function (req, res, next) {
-  res.render('mekan-detay', { 
-      'baslik':'Mekan Bilgisi',
-      'sayfaBaslik':'Starbucks',
-      'mekanBilgisi':{
-        'ad':'Starbucks',
-        'adres':'Centrum Garden',
-        'puan':4,
-        'imkanlar':['Kahve','Paste','Kek'],
-        'koordinatlar':{
-          'enlem':37.781885,
-          'boylam':30.566034
+  res.render('mekan-detay', {
+    'baslik': 'Mekan Bilgisi',
+    'footer': 'Furkan Tarhan 2020',
+    'sayfaBaslik': 'Starbucks',
+    'mekanBilgisi': {
+      'ad': 'Starbucks',
+      'adres': 'Centrum Garden',
+      'puan': 4,
+      'imkanlar': ['Kahve', 'Paste', 'Kek'],
+      'koordinatlar': {
+        'enlem': 37.781885,
+        'boylam': 30.566034
+      },
+      'saatler': [
+        {
+          'gunler': 'Pazartesi-Cuma',
+          'acilis': '7.00',
+          'kapanis': '23.00',
+          'kapali': false
         },
-        'saatler':[
-          {
-             'gunler':'Pazartesi-Cuma',
-             'acilis':'7.00',
-             'kapanis':'23.00',
-             'kapali':false
-          },
-          {
-            'gunler':'Cumartesi',
-            'acilis':'9.00',
-            'kapanis':'22.30',
-            'kapali':false
-          },
-          {
-            'gunler':'Pazar',         
-            'kapali':true
-          }
+        {
+          'gunler': 'Cumartesi',
+          'acilis': '9.00',
+          'kapanis': '22.30',
+          'kapali': false
+        },
+        {
+          'gunler': 'Pazar',
+          'kapali': true
+        }
       ],
-      'yorumlar':[
-          {
-            'yorumYapan':'Furkan Tarhan',
-            'puan':3,
-            'tarih':'1 Aralık 2020',
-            'yorumMetni':'Kahveler Enfessssss'
-          },
-          {
-            'yorumYapan':'Utku Dericioğlu',
-            'puan':5,
-            'tarih':'2 Aralık 2020',
-            'yorumMetni':'Buranın Kahveleri Harika!'
-          }
-       ]
+      'yorumlar': [
+        {
+          'yorumYapan': 'Furkan Tarhan',
+          'puan': 3,
+          'tarih': '1 Aralık 2020',
+          'yorumMetni': 'Kahveler Enfessssss'
+        },
+        {
+          'yorumYapan': 'Utku Dericioğlu',
+          'puan': 5,
+          'tarih': '2 Aralık 2020',
+          'yorumMetni': 'Buranın Kahveleri Harika!'
+        }
+      ]
     }
-    
-    
+
+
   });
 }
 
 
 const yorumEkle = function (req, res, next) {
-  res.render('yorum-ekle', { title: 'Yorum Ekle' });
+  res.render('yorum-ekle', {
+    title: 'Yorum Ekle',
+    'footer': 'Furkan Tarhan 2020',
+  });
 }
 
 module.exports = {
